7b800c4156f684ab20b0cee249ad71cd7bacf243
grantrisk <riskgrant@gmail.com>
2024-06-14 08:14:37 -0400

    feat: scroll animation component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import CycleText from "./components/CycleText";
 import InfoCard from "./components/InfoCard";
 import Image from "next/image";
-import Footer from "./components/Footer";
+import ScrollAnimationWrapper from "./components/ScrollAnimationWrapper";
 
 export default function Home() {
   return (
@@ -66,55 +66,69 @@ export default function Home() {
             />
           </div>
 
-          <p className={styles.textSection}>
-            I use Sjolie products. Their tanning solutions offer customizable
-            formulas for all skin types, allowing you to tailor your tan with
-            options for color intensity, hydration levels, and your choice of
-            personalized scents through spray tan enhancements.
-          </p>
+          <ScrollAnimationWrapper>
+            <p className={styles.textSection}>
+              I use Sjolie products. Their tanning solutions offer customizable
+              formulas for all skin types, allowing you to tailor your tan with
+              options for color intensity, hydration levels, and your choice of
+              personalized scents through spray tan enhancements.
+            </p>
+          </ScrollAnimationWrapper>
         </section>
 
         <section className={styles.prepSection}>
           <h2>Preparation</h2>
           <h5>Before Your Appointment</h5>
           <div className={styles.cardSection}>
-            <InfoCard
-              imageSrc={"/care_pictures/prep_1.png"}
-              text={
-                "Exfoliate 24 hrs prior to your spray tan appointment using a mitt and oil-free scrub."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/prep_2.png"}
-              text={
-                "Shaving and waxing should be done 24 hrs prior to your appointment."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/prep_3.png"}
-              text={
-                "Do not apply any oils, makeup, deodorant and perfume before the spray. Your skin should be " +
-                "free from any products."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/prep_4.png"}
-              text={
-                "Make sure you schedule all other spa treatments for before your spray tan, including facials, " +
-                "waxing, and mani/pedis."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/prep_5.png"}
-              text={
-                "During the session, it's up to you how much you want to undress, and I recommend you go with " +
-                "what makes you feel comfortable!"
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/prep_6.png"}
-              text={"Book your appointment 2 days before an event."}
-            />
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/prep_1.png"}
+                text={
+                  "Exfoliate 24 hrs prior to your spray tan appointment using a mitt and oil-free scrub."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/prep_2.png"}
+                text={
+                  "Shaving and waxing should be done 24 hrs prior to your appointment."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/prep_3.png"}
+                text={
+                  "Do not apply any oils, makeup, deodorant and perfume before the spray. Your skin should be " +
+                  "free from any products."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/prep_4.png"}
+                text={
+                  "Make sure you schedule all other spa treatments for before your spray tan, including facials, " +
+                  "waxing, and mani/pedis."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/prep_5.png"}
+                text={
+                  "During the session, it's up to you how much you want to undress, and I recommend you go with " +
+                  "what makes you feel comfortable!"
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/prep_6.png"}
+                text={"Book your appointment 2 days before an event."}
+              />
+            </ScrollAnimationWrapper>
             <Image
               src={"/sun_image.svg"}
               alt={"Sun Image"}
@@ -136,43 +150,55 @@ export default function Home() {
           <h2>After Care</h2>
           <h5>Tips To Maintain Your Glow</h5>
           <div className={styles.cardSection}>
-            <InfoCard
-              imageSrc={"/care_pictures/aftercare_1.png"}
-              text={
-                "To help your tan develop, avoid all moisture for at least 5 hrs after the session."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/aftercare_2.png"}
-              text={
-                "Wear loose-fitting, dark clothing so your tan doesn't rub off or look uneven."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/aftercare_3.png"}
-              text={
-                "When you shower, be gentle and only use oil-free cleansers. Pat yourself dry instead of rubbing."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/aftercare_4.png"}
-              text={
-                "After every shower, I highly recommend using a moisturizer to extend the life of your tan."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/aftercare_5.png"}
-              text={
-                "Avoid harsh chemicals such as BHA, salicylic acid, & anti-aging products."
-              }
-            />
-            <InfoCard
-              imageSrc={"/care_pictures/aftercare_6.png"}
-              text={
-                "When the tan starts to fade, it's essential to exfoliate the skin in order to remove the old" +
-                " tan residue."
-              }
-            />
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/aftercare_1.png"}
+                text={
+                  "To help your tan develop, avoid all moisture for at least 5 hrs after the session."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/aftercare_2.png"}
+                text={
+                  "Wear loose-fitting, dark clothing so your tan doesn't rub off or look uneven."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/aftercare_3.png"}
+                text={
+                  "When you shower, be gentle and only use oil-free cleansers. Pat yourself dry instead of rubbing."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/aftercare_4.png"}
+                text={
+                  "After every shower, I highly recommend using a moisturizer to extend the life of your tan."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/aftercare_5.png"}
+                text={
+                  "Avoid harsh chemicals such as BHA, salicylic acid, & anti-aging products."
+                }
+              />
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper>
+              <InfoCard
+                imageSrc={"/care_pictures/aftercare_6.png"}
+                text={
+                  "When the tan starts to fade, it's essential to exfoliate the skin in order to remove the old" +
+                  " tan residue."
+                }
+              />
+            </ScrollAnimationWrapper>
             <Image
               src={"/sun_image.svg"}
               alt={"Sun Image"}
