fe89c627411c194326126e5424f2072195a35572
grantrisk <riskgrant@gmail.com>
2024-07-16 08:21:01 -0400

    fix: resolved merge errors

--- app/(static)/services/page.tsx
+++ app/(static)/services/page.tsx
@@ -52,17 +52,17 @@ export default function Services() {
           <PricingCard
             title="Coconut Scent Drops"
             description="Add a tropical escape to your spray tan."
-            price={7}
+            price={5}
           />
           <PricingCard
             title="Pineapple Scent Drops"
             description="Infuse your spray tan with a refreshingly sweet aroma."
-            price={7}
+            price={5}
           />
           <PricingCard
             title="Orange Ginger Scent Drops"
             description="Enhance your spray tan with a zesty and invigorating scent."
-            price={7}
+            price={5}
           />
         </section>
 
